8635671c7dd92a0a84f0405e63245a6c28ab2799
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-07-06 22:00:17 -0500

    created login and register forms with no POST logic yet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App.jsx";
+import '../styles/tailwind.css'
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+
 // import React from "react";
-// import { createRoot } from "react-dom/client";
+// import ReactDOM from "react-dom";
 // import App from "./components/App";
 
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-// root.render(<App />);
-
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
