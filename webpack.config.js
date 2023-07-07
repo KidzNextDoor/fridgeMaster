15af4d4f78e702ebd2b72810c18f811c5e30824e
Arthur Griffith <arthur.griffith94@gmail.com>
2023-07-06 20:44:25 -0400

    moved babel and webpack files

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/build"),
+      publicPath: "/",
+    },
+    // historyApiFallback: true,
+    compress: true,
+    port: 8080,
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+  //This property defines where the application starts
+  entry: path.resolve(__dirname, "src", "index.js"),
+
+  //This property defines the file path and the file name which will be used for deploying the bundled file
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+
+  //Setup loaders
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  // Setup plugin to use a HTML file for serving bundled js files
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
