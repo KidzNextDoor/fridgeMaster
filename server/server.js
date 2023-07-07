83e65636db02f7483e8dde2113b71bece5a5f58c
Julia Xin <julia.zl.xin@gmail.com>
2023-07-06 22:14:20 -0400

    connect mongodb server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
@@ -14,6 +15,8 @@ app.use(express.static(path.resolve(__dirname, "../build")));
 app.get("/", (req, res) => {
   return res.status(200).sendFile(path.resolve(__dirname, "../index.html"));
 });
+
+mongoose.connect("mongodb+srv://jujubecadet5:eKgNmEUmLHoMh3F5@cluster0.lhvbuj6.mongodb.net/fridgewizard");
 
 const PORT = 3000;
 
