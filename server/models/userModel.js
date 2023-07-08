0dae30961920b8c02fc8282e5315b05f6142fd7a
Julia Xin <julia.zl.xin@gmail.com>
2023-07-08 14:37:57 -0400

    add preload data & modify user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = {
-    password: String,
-    email: String,
-    fridgeContents: {
-        
+    username: {type: String, required: true },
+    password: {type: String, required: true },
+    email: {type: String, required: true },
+    fridgeContents: [
+        {item: {type: String, required: true },
+        type: {type: String, required: true },
+        category: {type: String, required: true },
+        purchaseDate: {type: String, required: true },
+        shelfLife: {type: Number, required: true },
+        expDate: {type: String, required: true }
     }
+    ]
 }
 
 const UserData = mongoose.model("UserData", userSchema);
