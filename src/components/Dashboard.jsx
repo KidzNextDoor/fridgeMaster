13d75c9ee937e8ade5fa70bdebe3a4c729b608ed
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-07-08 12:25:18 -0500

    created dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <p>Welcome to your Dashboard</p>
+        </div>
+    );
+};
+
+export default Dashboard;
