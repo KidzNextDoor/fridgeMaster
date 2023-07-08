f80b1685719a49d14c5f92816fb150ee5d607380
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-07-08 16:53:39 -0500

    Homepage, Dashboard and Contents basic structure completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import {Login} from './Login';
 import { Register } from "./Register"; 
+import { Dashboard } from "./Dashboard";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Homepage } from "./Homepage";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
+  const [token, setToken] = useState(true);
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
+
+  if(!token) {
     return (
-      <div className="App">
-        {currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>}
-      </div>
-    );
+      currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+  )}
+  else{
+    return <Dashboard/>
+  }
+
+  // const toggleForm = (formName) => {
+  //   setCurrentForm(formName);
+  // }
+  //   return (
+  //     <div className="App">
+  //       <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/dashboard" element={<Dashboard/>}/>
+  //         <Route path="/login" element={currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>}/>
+  //       </Routes>
+  //     </BrowserRouter>
+  //     </div>
+  //   );
 }
 
 export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import logo from "../images/logo.png"
+import wizzardBuddy from "../images/wizzardBuddy.png"
+import expiringSoon from "../images/expiringSoon.png"
+import spoiled  from "../images/spoiled.png"
+import { Contents } from './Contents';
+//import types object from json object in db
 
-const Dashboard = () => {
+export const Dashboard = () => {
+    const [purchaseDate, setpurchaseDate] = useState('')
     return (
         <div>
-            <p>Welcome to your Dashboard</p>
+            <header>
+            <img className='logo' src={logo} alt="Logo" />
+            <h1>Fridge Wizzard</h1>
+            <button>Logout</button>
+            </header>
+            <button><img src={expiringSoon} alt="expiringSoon"/></button>
+            <button><img src={spoiled} alt="spoiled"/></button>
+            <form>
+                <h2>Add an item</h2>
+                <label htmlFor="purchaseDate">Date Purchased:</label>
+                <input className="input" value={purchaseDate} onChange={(e) => setpurchaseDate(e.target.value)}  type='date' placeholder="Date Purchased" id="purchaseDate" name="purchaseDate"/>
+                <label htmlFor='type'>Type:</label>
+                <input className='input' type='dropdown' name='type' id='type' placeholder='Enter Type'/>
+                <label htmlFor='expDate'>Expiration date:</label>
+                <input className='input' type='date' placeholder="Expiration Date" id="expDate" name="expDate"/>
+                <label htmlFor='itemName'>Name:</label>
+                <input className='input' type='text' placeholder='Item Name' id='itemName'name='itemName'/>
+                <button>placeholder for a button</button>
+            </form>
+            <Contents/>
         </div>
     );
 };
-
-export default Dashboard;
