bd5ee408fa8c233e7ecc736541590a284cb476aa
Gambarou <45675923+Gambarou@users.noreply.github.com>
2023-07-10 17:24:36 -0700

    Revert "Rh/login"

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  plugins: [],
-}
