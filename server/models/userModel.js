43e7ceab2c0f2473dd89dbc500c94d369f9c939e
Julia Xin <julia.zl.xin@gmail.com>
2023-07-12 19:54:48 -0400

    changes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,19 +1,56 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
 
 const userSchema = {
-    username: {type: String, required: true },
+    username: {type: String, required: true, index: { unique: true } },
     password: {type: String, required: true },
-    email: {type: String, required: true },
+    email: {type: String, required: true, index: { unique: true } }, 
     fridgeContents: [
-        {item: {type: String, required: true },
-        type: {type: String, required: true }, //other - value 0
-        category: {type: String, required: true },
-        shelfLife: {type: Number, required: true },
-        expDate: {type: String, required: true }
-    }
+        {
+            itemid: { type: String, default: uuidv4 },
+            item: { type: String, required: true },
+            name: { type: String },
+            category: { type: String, required: true },
+            expDate: {type: String, required: true }
+        }
     ]
 }
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+  
+    if (!user.isModified('password')) return next();
+  
+    try {
+      const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+  
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  })
+
+userSchema.statics.comparePassword = async function(password, hashedPassword) {
+  try{
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const UserData = mongoose.model("UserData", userSchema);
 
 module.exports = UserData;
+
+/*user settings stored in a different table
+suggested expiration date of today's date + shelf life but client can edit manually
+update existing shelflife.json? or keep as user-specific - create model for user-specific features
+*/
