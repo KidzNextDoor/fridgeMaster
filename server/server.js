164e9577f81da33d0fca2a24e6d2233eca03b535
Arthur Griffith <arthur.griffith94@gmail.com>
2023-07-12 22:02:33 -0400

    resolved merge conflicts

--- server/server.js
+++ server/server.js
@@ -44,43 +44,43 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err })
 });
 
-// starts the Mongo DB when the server is started
-require('dotenv').config();
-require('./db')
+// // starts the Mongo DB when the server is started
+// require('dotenv').config();
+// require('./db')
 
-// routers are defined here
-const apiRouter = require('./routes/apiRouter');
-const userRouter = require('./routes/userRoutes');
-const inventoryRouter = require('./routes/inventoryRoutes'); 
+// // routers are defined here
+// const apiRouter = require('./routes/apiRouter');
+// const userRouter = require('./routes/userRoutes');
+// const inventoryRouter = require('./routes/inventoryRoutes'); 
 
-const app = express();
+// const app = express();
 
-app.use(cors({ credentials: true, origin: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false}));
-app.use(express.static(path.resolve(__dirname, "../build")));
+// app.use(cors({ credentials: true, origin: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false}));
+// app.use(express.static(path.resolve(__dirname, "../build")));
 
-app.get("/", (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, "../index.html"));
-});
+// app.get("/", (req, res) => {
+//   return res.status(200).sendFile(path.resolve(__dirname, "../index.html"));
+// });
 
-// created a route file to keep code organized
-// all routes to /api/inventory go to inventoryRoutes
-app.use('/api/inventory', inventoryRouter);
-app.use("/api/users", userRouter);
-app.use('/api', apiRouter);
+// // created a route file to keep code organized
+// // all routes to /api/inventory go to inventoryRoutes
+// app.use('/api/inventory', inventoryRouter);
+// app.use("/api/users", userRouter);
+// app.use('/api', apiRouter);
 
-// catch all route for any unknown routes
-app.use('*', (req, res) => {
-  res.status(404).send('Page not found');
-});
+// // catch all route for any unknown routes
+// app.use('*', (req, res) => {
+//   res.status(404).send('Page not found');
+// });
 
-// create error handler to replace default express error handler
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ error: err })
-});
+// // create error handler to replace default express error handler
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   res.status(500).json({ error: err })
+// });
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
