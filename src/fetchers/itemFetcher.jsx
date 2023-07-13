7342f83aeb46cf64fe142c3c15adec21ab59ceaa
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-07-12 19:53:44 -0500

    quick commit before switching to dev

--- src/fetchers/itemFetcher.jsx
+++ src/fetchers/itemFetcher.jsx
@@ -6,9 +6,11 @@ export const getUser = () => {
 
 export const postFood = async () => {
     try {
-        const res = await axios.post('/', { purchaseDate, type, expDate, itemName });
+        const res = await axios.post('/inventory', { purchaseDate, type, expDate, itemName });
         return res.data;
       } catch (err) {
         console.log(err);
       }
 }
+
+//localStorage.getItem(email)
